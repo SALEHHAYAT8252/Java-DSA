c57f7092ee39a706329d7faa5a16677435d66b61
Saleh Hayat <salehhayat515@gmail.com>
2025-12-30 16:42:00 +0530

    Insertion Sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,26 +1,27 @@
 public class SelectionSort {
 
-    public static void selectionSort(int[] arr){
-        for(int i=0;i<arr.length-1;i++){
-            int sIdx=i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
-                    sIdx=j;
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int sIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    sIdx = j;
                 }
             }
-            if(sIdx!=i){
-            int temp=arr[i];
-            arr[i]=arr[sIdx];
-            arr[sIdx] = temp;
+            if (sIdx != i) {
+                int temp = arr[i];
+                arr[i] = arr[sIdx];
+                arr[sIdx] = temp;
             }
         }
     }
-    public static void main(String[] args){
-        int[] arr={5,8,7,9,6,10};
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 8, 7, 9, 6, 10 };
         selectionSort(arr);
         System.out.println("Sorted Array");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
